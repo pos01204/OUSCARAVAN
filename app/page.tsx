8bd35afaedade57a1919f2704c9703dc000b1801
pos01204 <jihoon@backpac.kr>
2026-01-05 21:53:09 +0900

    phase5

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation';
 
-export default function Home() {
-  redirect('/home');
+export default function RootPage() {
+  // 루트 페이지 접근 시 관리자 로그인 페이지로 리다이렉트
+  // 고객은 알림톡 링크를 통해 /guest/[token]으로 직접 접근
+  redirect('/login');
 }
