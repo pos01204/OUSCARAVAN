23b62ea1af9815a024b17c67ffee99d5809e2c2b
pos01204 <jihoon@backpac.kr>
2026-01-08 10:57:09 +0900

    하단 네비게이션 수정

--- components/shared/ConditionalFooter.tsx
+++ components/shared/ConditionalFooter.tsx
@@ -6,8 +6,8 @@ import { Footer } from './Footer';
 export function ConditionalFooter() {
   const pathname = usePathname();
   
-  // 게스트 페이지는 자체 레이아웃에서 Footer를 렌더링하므로 여기서는 제외
-  if (pathname?.startsWith('/guest/')) {
+  // 게스트 페이지와 관리자 페이지는 자체 레이아웃에서 Footer를 렌더링하므로 여기서는 제외
+  if (pathname?.startsWith('/guest/') || pathname?.startsWith('/admin/')) {
     return null;
   }
   
--- components/shared/ConditionalHeader.tsx
+++ components/shared/ConditionalHeader.tsx
@@ -6,8 +6,8 @@ import { Header } from './Header';
 export function ConditionalHeader() {
   const pathname = usePathname();
   
-  // 게스트 페이지는 자체 레이아웃에서 Header를 렌더링하므로 여기서는 제외
-  if (pathname?.startsWith('/guest/')) {
+  // 게스트 페이지와 관리자 페이지는 자체 레이아웃에서 Header를 렌더링하므로 여기서는 제외
+  if (pathname?.startsWith('/guest/') || pathname?.startsWith('/admin/')) {
     return null;
   }
   
